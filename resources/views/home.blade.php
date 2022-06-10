a2e6951da4bdb886cb8f9e256823cde9374b63c0
Desenvolvimento Pdvlopes <61085672+pdvlopes@users.noreply.github.com>
2022-06-10 18:12:49 -0300

    ajuste imagem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 </head>
 @include('Header')
 <body>
 
-
-    <img src="https://deverhum.com.br/wp-content/uploads/2019/05/Gest%C3%A3o-de-Compras.jpg" class="rounded-3" style="width: 100%;" alt="Avatar" />
+<div class='.container - fluid'>
+<img style="width: 100%;
+	height: 728px;
+	position: relative;
+	background-image: url(../img/banner.jpg) no-repeat;
+  background-size: 100%;"src="https://deverhum.com.br/wp-content/uploads/2019/05/Gest%C3%A3o-de-Compras.jpg" />
+</div>
+   
 
 
 </body>
