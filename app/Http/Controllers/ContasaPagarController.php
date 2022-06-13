6dbfca0b3e9e6d68e176fa6b0e73f771098b2e5a
Desenvolvimento Pdvlopes <61085672+pdvlopes@users.noreply.github.com>
2022-06-13 16:54:37 -0300

    LF-10
    Back-End Contas a pagar.

--- app/Http/Controllers/ContasaPagarController.php
+++ app/Http/Controllers/ContasaPagarController.php
@@ -3,82 +3,160 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ContasaPagar;
+use App\Http\Controllers\EmpresaController;
+use Illuminate\Support\Facades\DB;
 
 class ContasaPagarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+    $Empresa = new EmpresaController();
+    $Dados = $Empresa->Listar();
+
+    return view('ContasaPagar.ContasaPagar',['Empresas'=>$Dados]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        if (empty($request->Descricao)) {
+            echo "<script>
+              alert('Preencha a Descrição');
+            </script>";
+            exit;
+        }
+        if (empty($request->Vencimento)) {
+            echo "<script>
+              alert('Preencha o vencimento');
+            </script>";
+            exit;
+        }
+        if (empty($request->Dataemissao)) {
+            echo "<script>
+              alert('Preencha a Data da emissão');
+            </script>";
+            exit;
+        }
+        if (empty($request->Total)) {
+            echo "<script>
+              alert('Preencha a Total');
+            </script>";
+            exit;
+        }
+        if (empty($request->Datarecebimento)) {
+            echo "<script>
+              alert('Preencha a Data do recebimento');
+            </script>";
+            exit;
+        }
+        if (empty($request->CodEmpresa)) {
+            echo "<script>
+              alert('Preencha a Descrição');
+            </script>";
+            exit;
+        } else {
+            ContasaPagar::create([
+                'Barras' => $request->Barras,
+                'Descricao' => $request->Descricao,
+                'CodFornecedor' => $request->CodFornecedor,
+                'Total' => $request->Total,
+                'TotalDesconto' => $request->TotalDesconto,
+                'TotalAcréscimo' => $request->TotalAcrescimo,
+                'TotaldosProdutos' => $request->TotalProdutos,
+                'Vencimento' => $request->Vencimento,
+                'CodGrupo' => $request->CodGrupo,
+                'CodSubGrupo' => $request->CodSubGrupo,
+                'Parcelas' => $request->Parcelas,
+                'Dataemissao' => $request->Dataemissao,
+                'Datarecebimento' => $request->Datarecebimento,
+                'Boleta' => $request->Boleta,
+                'NotaFiscal' => $request->NotaFiscal,
+                'Serie' => $request->Serie,
+                'CodEmpresa' => $request->CodEmpresa,
+            ]);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $ContasaPagar = ContasaPagar::findOrFail($id);
+        return view('/ContasaPagar.Ver',['ContasaPagar'=>$ContasaPagar]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function Listartodos()
     {
-        //
+
+        $ContasaPagar = DB::table('contasa_pagars')->paginate(20);
+        return view('/ContasaPagar.Todos',['ContasaPagar'=>$ContasaPagar]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $ContasaPagar = ContasaPagar::findOrFail($id);
+
+        if (empty($request->Descricao)) {
+            echo "<script>
+              alert('Preencha a Descrição');
+            </script>";
+            exit;
+        }
+        if (empty($request->Vencimento)) {
+            echo "<script>
+              alert('Preencha o vencimento');
+            </script>";
+            exit;
+        }
+        if (empty($request->Dataemissao)) {
+            echo "<script>
+              alert('Preencha a Data da emissão');
+            </script>";
+            exit;
+        }
+        if (empty($request->Total)) {
+            echo "<script>
+              alert('Preencha a Total');
+            </script>";
+            exit;
+        }
+        if (empty($request->Datarecebimento)) {
+            echo "<script>
+              alert('Preencha a Data do recebimento');
+            </script>";
+            exit;
+        }
+        if (empty($request->CodEmpresa)) {
+            echo "<script>
+              alert('Preencha a Descrição');
+            </script>";
+            exit;
+        } else {
+            $ContasaPagar->Update([
+                'Barras' => $request->Barras,
+                'Descricao' => $request->Descricao,
+                'CodFornecedor' => $request->CodFornecedor,
+                'Total' => $request->Total,
+                'TotalDesconto' => $request->TotalDesconto,
+                'TotalAcréscimo' => $request->TotalAcrescimo,
+                'TotaldosProdutos' => $request->TotalProdutos,
+                'Vencimento' => $request->Vencimento,
+                'CodGrupo' => $request->CodGrupo,
+                'CodSubGrupo' => $request->CodSubGrupo,
+                'Parcelas' => $request->Parcelas,
+                'Dataemissao' => $request->Dataemissao,
+                'Datarecebimento' => $request->Datarecebimento,
+                'Boleta' => $request->Boleta,
+                'NotaFiscal' => $request->NotaFiscal,
+                'Serie' => $request->Serie,
+                'CodEmpresa' => $request->CodEmpresa,
+            ]);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $ContasaPagar = ContasaPagar::findOrFail($id);
+        $ContasaPagar->delete();
+
+        return "<script>alert('Deletado com sucesso.');location = '/ContasaPagar/Todos';</script>";
     }
 }
