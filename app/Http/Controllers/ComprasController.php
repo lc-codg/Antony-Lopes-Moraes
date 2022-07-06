e4ff7f60fa372d0489766f4faf6b88b15dba8e81
Desenvolvimento Pdvlopes <61085672+pdvlopes@users.noreply.github.com>
2022-07-06 17:15:58 -0300

    LF-8
    parte 1

--- app/Http/Controllers/ComprasController.php
+++ app/Http/Controllers/ComprasController.php
@@ -3,82 +3,133 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Compras;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use App\Classes\ObterDados;
+use App\Http\Controllers\ItensController;
+use Exception;
 
 class ComprasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function Show()
     {
-        //
+        if (session()->has('CartCompras'))
+            $Carrinho = (Session::get('CartCompras'));
+        else
+            $Carrinho = session('CartCompras', []);
+
+        if (session()->has('Fornecedor')) {
+            $Fornecedor = Session::get('Fornecedor');
+        } else {
+            $Fornecedor = session('Fornecedor', ['Razao' => '', 'Cnpj' => '', 'Id' => '']);
+        }
+        $ObterDados = new ObterDados();
+        $Empresa = $ObterDados->ListaDeEmpresas();
+
+        return view('Compras.Carrinho', ['CartCompras' => $Carrinho, 'Fornecedor' => $Fornecedor, 'Empresa' => $Empresa]);
+    }
+    public function LimparCarrinho()
+    {
+        Session::flush('CartComprasCompras');
+        Session::flush('Fornecedor');
+        return "<script>location='/Compras/Carrinho';</script>";
+    }
+    public function Delete($id)
+    {
+        $Compras = Compras::findOrfail($id);
+        $Compras->delete();
+        return "<script>alert('Deletado com sucesso!');location='/Compras/Todos';</script>";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function ListarPorId($Id)
     {
-        //
+        $Pedido = Compras::findOrfail($Id);
+        return view('Compras.Ver', ['Compras' => $Pedido]);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function ListarTodos(Request $request)
     {
-        //
+        $Dados = new ObterDados;
+        $Empresa = $Dados->ListaDeEmpresas();
+        $Fornecedor = $Dados->ListaDeFornecedores();
+
+        $Compras = DB::table('Compras')->select(
+            'Compras.id',
+            'Compras.CodigoDoFornecedor',
+            'Compras.Total',
+            'Compras.TotalDesconto',
+            'Compras.TotalAcréscimo',
+            'Compras.DtPedido',
+            'Compras.CodEmpresa',
+            'Fornecedors.Nome',
+            'empresas.Razao',
+
+        )->
+        join('Fornecedors', 'Compras.CodigoDoFornecedor', '=', 'Fornecedors.id')->
+        join('empresas', 'Compras.CodEmpresa','=','empresas.id')->
+        where('empresas.Razao', 'LIKE', '%'.$request->Nome.'%')->
+        orwhere('Fornecedors.Nome','LIKE','%'.$request->Nome.'%')->
+        orwhere('Fornecedors.Razao','LIKE','%'.$request->Nome.'%')->
+        whereBetween('DtPedido',array($request->Dataini,$request->Datafim))->
+        paginate(20);
+
+        return view('Compras.Todos', ['Compras' => $Compras,'Empresa'=>$Empresa,'Fornecedor'=>$Fornecedor]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function VerificaDados($Fornecedor, $Empresa, $Produtos)
     {
-        //
+        if (empty($Fornecedor['Id'])) {
+            echo "<script>alert('Preencha o Fornecedor.'),history.back()</script>";
+            exit;
+        }
+        if (empty($Empresa['Id'])) {
+
+            echo "<script>alert('Preencha o emitente.'),history.back()</script>";
+            exit;
+        }
+        if ($Produtos == 0) {
+            echo "<script>alert('Insira produtos ao pedido.'),history.back()</script>";
+            exit;
+        } else {
+
+            return true;
+        }
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function create(Request $request)
     {
-        //
-    }
+        $Fornecedor = session::get('Fornecedor');
+        $Empresa = session::get('Empresa');
+        $Itens = new ItensController;
+        $Total = 0;
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        if ($request->session()->has('CartCompras')) {
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+            $Produtos = session::get('CartCompras');
+            foreach ($Produtos as $row) {
+                $Total += ($row['Valor'] *  $row['Quantidade']);
+            }
+        } else {
+            $Produtos = 0;
+        }
+
+        if ($this->VerificaDados($Fornecedor, $Empresa, $Produtos)) {
+            $Compras = Compras::create([
+                'CodigoDoFornecedor' => $Fornecedor['Id'],
+                'Total' => $Total,
+                'TotaldosProdutos' => $Total,
+                'DtPedido' => date('Y-m-d'),
+                'Dataemissao' => date('Y-m-d'),
+                'DataSaida' => date('Y-m-d'),
+                'Finalidade' => 'Venda',
+                'CodEmpresa' => $Empresa['Id']
+            ]);
+
+            $Id = $Compras->id;
+
+            if ($Itens->Salvar($Produtos, $Id)) {
+                return "<script>alert('Pedido Salvo com sucesso.'),location='LimparCarrinho'</script>";
+            } else {
+                return "<script>alert(Erro ao Gravar.)</script>";
+            }
+        }
     }
 }
--- app/Http/Controllers/ItensCompraController.php
+++ app/Http/Controllers/ItensCompraController.php
@@ -3,82 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ItensCompras;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Facades\DB;
+use app\Console\Kernel;
+use App\Exceptions\InvalidOrderException;
+use Exception;
 
 class ItensCompraController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function Delete($id)
     {
-        //
+        $Iten = ItensCompras::findOrfail($id);
+        $Iten->delete();
+        return "<script>alert('Deletado com sucesso!');location='/ItensCompras/Todos';</script>";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function ListarPorId($id)
     {
-        //
+        $Iten = ItensCompras::findOrfail($id);
+        return view('ItensCompras.Todos', ['Itens' => $Iten]);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function ListarTodos($id)
     {
-        //
+        $itens = DB::table('itens_compras')->where('NumeroDoPedido', '=', $id)->paginate(20);
+        return view('ItensCompras.Todos', ['itens' => $itens]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function Salvar($Produtos, $Id)
     {
-        //
-    }
+        foreach ($Produtos as $row) {
+            ItensCompras::Create(
+                [
+                    'Descricao' => $row['Descricao'],
+                    'Barras' => $row['Barras'],
+                    'ValorUnitario' => $row['Valor'],
+                    'Quantidade' => $row['Quantidade'],
+                    'Subtotal' => ($row['Valor'] * $row['Quantidade']),
+                    'NumeroDoPedido' => $Id,
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+                ]
+            );
+        }
+        try {
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
